d5f6d9bf4c1e3b4af23af837d3b0115b2799b0f3
priyaverma01 <priyav8815@gmail.com>
2025-05-21 18:57:14 +0530

    added rolldice test

--- src/__tests_/RollDice.test.jsx
+++ src/__tests_/RollDice.test.jsx
@@ -2,3 +2,14 @@ import React from "react";
 import {render,fireEvent,screen} from '@testing-library/react'
 
 
+import RollDice from '../Components/RollDice';
+const mockOnRoll = jest.fn();
+const mockResetScore = jest.fn();
+
+test('renders "Click on dice to roll" text', () => {
+  render(<RollDice onRoll={mockOnRoll} resetScore={mockResetScore} />);
+  
+  const instructionText = screen.getByText(/Click on dice to roll/i);
+  expect(instructionText).toBeInTheDocument();
+});
+
